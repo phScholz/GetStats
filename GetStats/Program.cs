56f3dee78a48b3546c5e875dcab0d0240030f8b5
Philipp Scholz <pscholz@outlook.de>
2017-11-09 17:25:00 +0100

    Projektdateien hinzufügen.

---
+++ GetStats/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
